7849dfaf8c5fd96625f88589015079454f57f56c
LunexCoding <lunexcoding@vk.com>
2021-11-07 22:35:09 +0700

    Link check, checking internet connection

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
 import requests
+from requests import ConnectionError
 from debug import LOG_WARNING
 from const import protocols
 
@@ -11,17 +12,23 @@ class Downloader:
         self.downloadFile()
 
     def downloadFile(self):
-        if self._url.split('://')[0] not in protocols:
-            if self._url == '':
-                invalidURL = "''"
-                return LOG_WARNING(f'Invalid URL {invalidURL} . Perhaps you meant http://{self._url}?')
-            invalidURL = self._url.split('://')[1]
-            return LOG_WARNING(f'Invalid URL {self._url}. \nPerhaps you meant http://{invalidURL}?')
+        try:
+            if self._url.split('://')[0] not in protocols:
+                if self._url == '':
+                    invalidURL = "''"
+                    return LOG_WARNING(f'Invalid URL {invalidURL} . Perhaps you meant http://{self._url}?')
+                invalidURL = self._url.split('://')[1]
+                return LOG_WARNING(f'Invalid URL {self._url}. \nPerhaps you meant http://{invalidURL}?')
 
-        else:
-            self._request = requests.get(self._url)
-            if self._request.status_code == 200:
-                with open(self._filename, 'wb') as f:
-                    f.write(self._request.content)
             else:
-              LOG_WARNING('Unknown error {} '.format(self._request.status_code))
+                try:
+                    self._request = requests.get(self._url)
+                    if self._request.status_code == 200:
+                        with open(self._filename, 'wb') as f:
+                            f.write(self._request.content)
+                    else:
+                      LOG_WARNING('Unknown error {} '.format(self._request.status_code))
+                except ConnectionError:
+                    LOG_WARNING(f"Internet connection not detected.")
+        except IndexError:
+            LOG_WARNING('Invalid URL.')
